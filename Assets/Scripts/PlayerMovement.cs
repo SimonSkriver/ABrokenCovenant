4415c0e835e7224814c16bf2ccb74e37567249b2
Rune <runestenn@gmail.com>
2026-03-03 17:48:56 +0100

    Added blockout scene + temp FP controller
    Added a scene with v01 blockout and a FP controller prefab for testing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    [SerializeField] CharacterController controller;
+    [SerializeField] float movementSpeed = 10f; //potentially make two speed values. One for forward and one for left/right, so that strafing is a bit slower?
+    [SerializeField] float jumpHeight = 3f;
+
+    [SerializeField] float gravity = -9.81f;
+    Vector3 velocity;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    void Update()
+    {
+        
+        //Jump
+        if (controller.isGrounded && jumpAction.IsPressed())
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); //fiddle with jump so that it's less floaty. Needs to jump faster without jumping higher
+        }
+
+        //Keep player grounded, by forcing slight negative downward velocity
+        if(controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+
+
+        //Movement
+        Vector2 inputValue = moveAction.ReadValue<Vector2>();
+        Vector3 horizontalMovement = (transform.right * inputValue.x + transform.forward * inputValue.y) * movementSpeed;
+        horizontalMovement = Vector3.ClampMagnitude(horizontalMovement, 1f);
+        
+        //Combine vertical and horizontal movement
+        Vector3 unifiedMovement = (horizontalMovement * movementSpeed) + (velocity.y * Vector3.up);
+        controller.Move(unifiedMovement * Time.deltaTime);
+    }
+}
