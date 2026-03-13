35d8d8adfd942f34ec90e56f251b7ef59fabed39
JakobStorm7913 <156937915+JakobStorm7913@users.noreply.github.com>
2026-03-13 13:40:03 +0100

    Added virtualcamera + started new rotate script

---
+++ Assets/Scripts/V2RotatePuzzle.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class V2RotatePuzzle : MonoBehaviour, IInteractable
+{
+    [SerializeField] Transform playerAnchor;
+    [SerializeField] Transform horizontalPivot;
+    [SerializeField] Transform horizontalAim;
+    [SerializeField] Transform verticalPivot;
+    [SerializeField] Transform verticalAim;
+
+    private static RotatePuzzle activePuzzle;
+    
+    void Awake()
+    {
+        horizontalAim = GameObject.FindGameObjectWithTag("Player").transform;
+        verticalAim = GameObject.FindGameObjectWithTag("PlayerCam").transform;
+    }
+
+    void Update()
+    {
+        if (PlayerMovement.Instance.currentState == PlayerState.InPuzzle && activePuzzle == this)
+        {
+            DoPuzzle();
+        }
+    }
+
+    public void Interact(GameObject obj)
+    {
+        activePuzzle = this;
+        Debug.Log("Doing puzzle");
+        PlayerMovement.Instance.currentState = PlayerState.InPuzzle;
+        horizontalAim.rotation = Quaternion.LookRotation(horizontalPivot.forward);
+        verticalAim.rotation = Quaternion.LookRotation(verticalPivot.forward);
+    }
+
+    //Skal også fikses, at man ikke kigger i den vej vertical pivot kigger, når man starter puzzle
+
+    void DoPuzzle()
+    {
+        //Set player position to the anchor
+        PlayerMovement.Instance.transform.position = playerAnchor.position;
+
+        //Rotate the mirror based on camera and player rotation
+        horizontalPivot.rotation = Quaternion.Euler(horizontalPivot.eulerAngles.x, horizontalAim.eulerAngles.y, horizontalPivot.eulerAngles.z);
+        verticalPivot.rotation = Quaternion.Euler(verticalAim.eulerAngles.x, verticalPivot.eulerAngles.y, verticalPivot.eulerAngles.z);
+    }
+
+    public void Drop()
+    {
+        activePuzzle = null;
+        PlayerMovement.Instance.currentState = PlayerState.Normal;
+    }
+}
