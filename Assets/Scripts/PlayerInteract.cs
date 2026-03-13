480eefc16b8e5cbc9aed69b487087c667f232b81
SimonSkriver <simon.skriver@hotmail.com>
2026-03-13 09:44:49 +0100

    Update mirror and pedestal prefab

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -18,24 +18,54 @@ public class PlayerInteract : MonoBehaviour
         HandleInteract();
     }
 
+    [SerializeField] float interactRadius = 0.25f;
+
     void HandleInteract()
     {
         if (interactAction.WasPressedThisFrame() && PlayerMovement.Instance.currentState == PlayerState.Normal)
         {
-            if (Physics.Raycast(eyes.position, eyes.forward, out RaycastHit hit, reach))
+            Debug.Log("Interact was called");
+            RaycastHit hit;
+            bool interacted = false;
+
+            // First try a precise raycast
+            if (Physics.Raycast(eyes.position, eyes.forward, out hit, reach))
             {
-                interactable = hit.collider.GetComponent<IInteractable>();
-                if (interactable != null)
-                {
-                    interactable.Interact(hit.collider.gameObject);
-                }
+                interacted = TryInteract(hit);
+            }
+
+            // If no hit on precise ray, try a more forgiving spherecast
+            if (!interacted && Physics.SphereCast(eyes.position, interactRadius, eyes.forward, out hit, reach))
+            {
+                interacted = TryInteract(hit);
+            }
+
+            // Optionally highlight or log when no target found
+            if (!interacted)
+            {
+                Debug.Log("No interactable target in reach");
             }
         }
         
         else if (interactAction.WasPressedThisFrame() && (PlayerMovement.Instance.currentState == PlayerState.IsCarrying || PlayerMovement.Instance.currentState == PlayerState.InPuzzle))
         {
             Debug.Log("Dropping");
-            interactable.Drop();
+            if (interactable != null)
+            {
+                interactable.Drop();
+            }
         }
-    }  
+    }
+
+    private bool TryInteract(RaycastHit hit)
+    {
+        var target = hit.collider.GetComponent<IInteractable>();
+        if (target != null)
+        {
+            interactable = target;
+            interactable.Interact(hit.collider.gameObject);
+            return true;
+        }
+        return false;
+    }
 }
