4938b5061be3166ac9d2cb6796dcf7e7d57fa684
JakobStorm7913 <156937915+JakobStorm7913@users.noreply.github.com>
2026-03-20 15:16:12 +0100

    fixedGameFinishedPanel script

--- Assets/Scripts/ChurchDoor.cs
+++ Assets/Scripts/ChurchDoor.cs
@@ -1,20 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChurchDoor : MonoBehaviour
 {
+    [SerializeField] private GameObject gameOverPanel;
     private Animator whiteAnimator;
     private Animator textAnimator;
 
     void Start()
     {
-        whiteAnimator = GameObject.FindGameObjectWithTag("FadeToWhite").GetComponentInChildren<Animator>(true);
-        textAnimator = GameObject.FindGameObjectWithTag("Text").GetComponentInChildren<Animator>(true);
+        //whiteAnimator = GameObject.FindGameObjectWithTag("FadeToWhite").GetComponentInChildren<Animator>(true);
+        //textAnimator = GameObject.FindGameObjectWithTag("Text").GetComponentInChildren<Animator>(true);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            gameOverPanel.SetActive(true);
+            whiteAnimator = GameObject.FindGameObjectWithTag("FadeToWhite").GetComponentInChildren<Animator>(true);
+            textAnimator = GameObject.FindGameObjectWithTag("Text").GetComponentInChildren<Animator>(true);
             PlayerMovement.Instance.currentState = PlayerState.LockPlayer;
             whiteAnimator.SetTrigger("FadeToWhite");
             Invoke("ShowText", 2f);
