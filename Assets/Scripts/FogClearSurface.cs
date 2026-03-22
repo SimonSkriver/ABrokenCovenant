ffaff64e13d979e46c0b3e95133892689146d79d
JakobStorm7913 <156937915+JakobStorm7913@users.noreply.github.com>
2026-03-22 13:22:46 +0100

    fogcollider script update

--- Assets/Scripts/FogClearSurface.cs
+++ Assets/Scripts/FogClearSurface.cs
@@ -5,6 +5,7 @@ public class FogClearSurface : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject particleParent;
+    [SerializeField] private GameObject fogCollider;
     [SerializeField] private ParticleSystem[] particleSystems;
     void Start()
     {
@@ -19,6 +20,7 @@ public class FogClearSurface : MonoBehaviour
             main.loop = true;
             particle.Play(true);
         }
+        fogCollider.SetActive(true);
     }
 
     public void DisableParticles()
@@ -28,5 +30,6 @@ public class FogClearSurface : MonoBehaviour
             var main = particle.main;
             main.loop = false;
         }
+        fogCollider.SetActive(false);
     }
 }
