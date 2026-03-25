6c3231cf66e0dacee911b9eb070b50ab03bda67c
JakobStorm7913 <156937915+JakobStorm7913@users.noreply.github.com>
2026-03-25 19:15:12 +0100

    Updated and added comments

--- Assets/Scripts/CrazyF(r)og/FogClearSurface.cs
+++ Assets/Scripts/CrazyF(r)og/FogClearSurface.cs
@@ -3,37 +3,40 @@ using UnityEngine;
 public class FogClearSurface : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private GameObject particleParent;
+    [SerializeField] private GameObject particleParent; // Set CrazyFogParticle GameObject in Inspector
     [SerializeField] private GameObject fogCollider;
-    [SerializeField] private GameObject fogEffect;
+    [SerializeField] private GameObject fogEffect; // Set CrazyFogEffect parent GameObject in Inspector
     [SerializeField] private ParticleSystem[] particleSystems;
 
     void Start()
     {
+        // Collect all particle systems from parent
         particleSystems = particleParent.GetComponentsInChildren<ParticleSystem>();
     }
 
     public void EnableParticles()
     {
+        // Enable loop and start particle effect
         foreach (ParticleSystem particle in particleSystems)
         {
             var main = particle.main;
             main.loop = true;
             particle.Play(true);
         }
-
+        // Enable collider and CrazyFogEffect GameObject
         fogCollider.SetActive(true);
         fogEffect.SetActive(true);
     }
 
     public void DisableParticles()
     {
+        //Disable loop
         foreach (ParticleSystem particle in particleSystems)
         {
             var main = particle.main;
             main.loop = false;
         }
-
+        // Remove collider and disable CrazyFogEffect
         fogCollider.SetActive(false);
         fogEffect.SetActive(false);
     }
--- Assets/Scripts/Puzzle/LockedMirrorSurface.cs
+++ Assets/Scripts/Puzzle/LockedMirrorSurface.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class LockedMirrorSurface : MonoBehaviour
 {
-    
     [SerializeField] private Transform beamEmitter; // GameObject on prefab used to customize next start and endpoint of next RayCast hit.
     [SerializeField] private AudioSource mirrorSFXSource; // AudioSource that plays the mirror hit sfx 
-    
 
     // Public get for MultiBeamScript to access Transform
     public Transform GetEmitterTransform()
